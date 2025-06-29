12887fbea1e54ef1eaa6f35b7b7de36d468688ee
Bubble <2425569471@qq.com>
2025-06-29 19:55:00 +0800

    Object Pool

---
+++ Utils/ObjectPool.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BubbleZun.Utils
+{
+    public class ObjectPool
+    {
+        GameObject prefab;
+        List<GameObject> pool = new List<GameObject>();
+        public ObjectPool(GameObject prefab)
+        {
+            this.prefab = prefab;
+        }
+        public GameObject GetObject(Transform parent)
+        {
+            GameObject obj = null;
+            while (obj == null && pool.Count > 0)
+            {
+                obj = pool[pool.Count - 1];
+                pool.RemoveAt(pool.Count - 1);
+            }
+            if (obj == null)
+            {
+                obj = GameObject.Instantiate(prefab);
+                BDebug.Log("Pool drained, Instantiate " + obj.name);
+            }
+            obj.transform.SetParent(parent);
+            obj.SetActive(true);
+            return obj;
+        }
+        public void Recycle(GameObject obj)
+        {
+            obj.SetActive(false);
+            pool.Add(obj);
+        }
+    }
+}
