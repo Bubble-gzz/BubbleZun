951bd47dafb03469beacd1bb462d6c0cb0f29ced
Bubble <2425569471@qq.com>
2025-07-21 22:31:03 +0800

    InputManager: + InputField屏蔽快捷键

--- Utils/InputManager.cs
+++ Utils/InputManager.cs
@@ -2,12 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+using BubbleZun.Utils;
 
 public class InputManager : Singleton<InputManager>
 {
     public UnityEvent OnLMBDown = new UnityEvent();
+    Dictionary<KeyCode, UnityEvent> keyEvents = new Dictionary<KeyCode, UnityEvent>();
+    bool IsEditingInputField()
+    {
+        GameObject current = EventSystem.current.currentSelectedGameObject;
+        if (current == null) return false;
+        return current.GetComponent<InputField>() != null || current.GetComponent<TMP_InputField>() != null;
+    }
     void Start()
     {
         
+    }
+    void Update()
+    {
+        if (!IsEditingInputField()) {
+            foreach (var keyEvent in keyEvents) {
+                if (Input.GetKeyDown(keyEvent.Key)) {
+                    keyEvent.Value.Invoke();
+                }
+            }
+        }
+    }
+    public void AddListener(KeyCode key, UnityAction callback)
+    {
+        if (!keyEvents.ContainsKey(key)) {
+            keyEvents[key] = new UnityEvent();
+        }
+        keyEvents[key].RemoveListener(callback);
+        keyEvents[key].AddListener(callback);
+    }
+    public void RemoveListener(KeyCode key, UnityAction callback)
+    {
+        if (keyEvents.ContainsKey(key)) {
+            keyEvents[key].RemoveListener(callback);
+        }
     }
 }
