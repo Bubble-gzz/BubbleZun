8f0a2d19f7ac860cf89e94d9a6d9743b46278b82
Bubble <2425569471@qq.com>
2025-08-14 17:13:47 +0800

    rouletteSelector: + support undefined option

--- Interaction/Widgets/RouletteSelector.cs
+++ Interaction/Widgets/RouletteSelector.cs
@@ -7,6 +7,7 @@ namespace BubbleZun.Interaction.Widgets
 {
 public class RouletteSelector : MonoBehaviour
 {
+    public static string mixedOption = "-";
     [SerializeField] TMP_Text text;
     [SerializeField] List<string> options;
     public UnityEvent<string> onSelect;
@@ -19,20 +20,33 @@ public class RouletteSelector : MonoBehaviour
     }
     public void TurnLeft()
     {
-        selectedIndex--;
-        if (selectedIndex < 0) selectedIndex = options.Count - 1;
+        if (selectedIndex == -1) selectedIndex = 0;
+        else {
+            selectedIndex--;
+            if (selectedIndex < 0) selectedIndex = options.Count - 1;
+        }
         UpdateSelectedItem();
     }
     public void TurnRight()
     {
-        selectedIndex++;
-        if (selectedIndex >= options.Count) selectedIndex = 0;
+        if (selectedIndex == -1) selectedIndex = 0;
+        else {
+            selectedIndex++;
+            if (selectedIndex >= options.Count) selectedIndex = 0;
+        }
         UpdateSelectedItem();
     }
+
     void UpdateSelectedItem()
     {
-        text.text = options[selectedIndex];
-        onSelect.Invoke(options[selectedIndex]);
+        if (selectedIndex == -1) {
+            text.text = mixedOption;
+            onSelect.Invoke(mixedOption);
+        }
+        else {
+            text.text = options[selectedIndex];
+            onSelect.Invoke(options[selectedIndex]);
+        }
     }
     public void AddOption(string option)
     {
@@ -40,7 +54,7 @@ public class RouletteSelector : MonoBehaviour
     }
     public void Select(int index)
     {
-        if (index < 0 || index >= options.Count) return;
+        if ((index < 0 || index >= options.Count) && index != -1) return;
         selectedIndex = index;
         UpdateSelectedItem();
     }
