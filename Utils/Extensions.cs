1da428c472537694bf1595aab7eaa4814e7b6c37
Bubble <2425569471@qq.com>
2025-08-19 18:49:39 +0800

    Color: + extension

--- Utils/Extensions.cs
+++ Utils/Extensions.cs
@@ -26,4 +26,11 @@ namespace BubbleZun.Utils{
             return null;
         }
     }
+    public static class ColorExtensions
+    {
+        public static Color SetAlpha(this Color color, float alpha)
+        {
+            return new Color(color.r, color.g, color.b, alpha);
+        }
+    }
 }
